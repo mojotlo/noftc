b50448ef444b7ce2ba8140836a947ba504109810
Taylor Jackson <tsjackson@gmail.com>
2013-01-30 00:09:42 -0600

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def email
+  end
+  
+  def restaurant
+    
+  end   
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noftc::Application.config.secret_token = 'adf944f19deb5778d528540f9bee8045c9a197f527afbb29914372141bffcdfe6a256328eccceb9874811f159a01c5111751347259f67867bf8f2ccddb2c0b5a'
